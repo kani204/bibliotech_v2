06ec0d7df0ea020c574116fcd7614d0830db734a
Exaedro <owomolo123@gmail.com>
2024-08-19 10:57:45 -0300

    act

--- src/web/public/js/cambiarTema.js
+++ src/web/public/js/cambiarTema.js
@@ -1,1 +1,46 @@
-document.documentElement.setAttribute('tema', 'light')
+const sol = document.getElementById('sol')
+// const navbarMenu = document.getElementById('v')
+
+let currentTheme = getDefaultTheme();
+setTheme(currentTheme);
+
+sol.addEventListener('click', cambiarTema)
+
+function cambiarTema(elem) {
+    const newTheme = getNewTheme(currentTheme)
+	setTheme(newTheme)
+	currentTheme = newTheme
+    saveTheme(newTheme)
+}
+
+function getNewTheme(theme) {
+	return theme === 'dark' ? 'light' : 'dark'
+}
+
+function setTheme(theme) {
+  const html = document.documentElement
+  html.setAttribute('tema', theme)
+
+  if(theme == 'light') {
+    sol.src = '/img/sol-negro.svg'
+  }
+  if(theme == 'dark') sol.src = '/img/sol.svg'
+}
+
+function getSavedTheme() {
+  return localStorage.getItem('tema')
+}
+
+function saveTheme(theme) {
+  localStorage.setItem('tema', theme)
+
+  if(theme == 'light') sol.src = '/img/sol-negro.svg'
+  if(theme == 'dark') sol.src = '/img/sol.svg'
+}
+
+function getDefaultTheme() {
+    const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+    const systemSettingTheme = systemSettingDark.matches ? "dark" : "light";
+    const savedTheme = getSavedTheme();
+    return savedTheme ? savedTheme : systemSettingTheme;
+}
